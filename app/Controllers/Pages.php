ae19df4c5174ee064d4ba0f390626e1f99d0802e
Sultoni Rahman <sultonirahman85@gmail.com>
2022-08-03 17:04:01 +0700

    menghapus footer dan header yang ada di folder layout, mengubah file routes.php, pages.php, mengubah view about dan home, menambahkan file baru, navbar, template, contact

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -7,13 +7,10 @@ class Pages extends BaseController
     public function index()
     {
         $data = [
-            'title' => 'Home | Sultoni Rahman',
-            'test' => ["satu", "dua", "tiga", "empat"]
+            'title' => 'Home | Sultoni Rahman'
         ];
 
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
 
     public function about()
@@ -22,8 +19,27 @@ class Pages extends BaseController
             'title' => 'About | Sultoni Rahman'
         ];
 
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+        return view('pages/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => "Dusun Mekarsari",
+                    'kota' => "Bandung"
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => "Dusun Tanjungsari",
+                    'kota' => "Bandung"
+                ]
+            ]
+        ];
+
+        return view('pages/contact', $data);
     }
 }
--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,8 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
         <div class="col">
             <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, pariatur deserunt! Architecto explicabo qui officia voluptate. At architecto sed, possimus repudiandae deserunt reiciendis sapiente nisi facere dolor neque nulla ab?</p>
+
         </div>
     </div>
 </div>
+<?= $this->endSection(); ?>
